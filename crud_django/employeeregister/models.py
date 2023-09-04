12df49407c039ecd8fd79c1c4d2066432e8bb56a
4rkhamknight01 <devanshdasindia@outlook.com>
2023-09-04 20:29:50 +0530

    commiting

--- crud_django/employeeregister/models.py
+++ crud_django/employeeregister/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class position(models.Model):
+    title = models.CharField(max_length=20)
+
+class Employee(models.Model):
+    fullname = models.CharField(max_length=50)
+    emp_code = models.CharField(max_length=3)
+    mobile_no = models.CharField(max_length=10)
+    position = models.ForeignKey(position, on_delete=models.CASCADE)
