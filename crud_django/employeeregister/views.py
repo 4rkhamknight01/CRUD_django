2f56ca44f4ae88d353828649773f3149ff970c28
4rkhamknight01 <devanshdasindia@outlook.com>
2023-09-07 20:29:30 +0530

    sdvw

--- crud_django/employeeregister/views.py
+++ crud_django/employeeregister/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def employee_list(request):
-    return
+    return render(request, "employee_register/employee_list.html")
 
 
 # get and post api's from form
 def employee_form(request):
-    return
+    return render(request, "employee_register/employee_form.html")
 
 #deleting a record
 def employee_delete(request):
