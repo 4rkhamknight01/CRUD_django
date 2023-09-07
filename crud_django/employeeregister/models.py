61264c166316579a40860a3f58a3678adc93cf14
4rkhamknight01 <devanshdasindia@outlook.com>
2023-09-07 20:39:45 +0530

    sdv

--- crud_django/employeeregister/models.py
+++ crud_django/employeeregister/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class position(models.Model):
+class Position(models.Model):
     title = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.title
 
 class Employee(models.Model):
     fullname = models.CharField(max_length=50)
     emp_code = models.CharField(max_length=3)
     mobile_no = models.CharField(max_length=10)
-    position = models.ForeignKey(position, on_delete=models.CASCADE)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
--- crud_django/employeeregister/views.py
+++ crud_django/employeeregister/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-
+from .forms import EmployeeForm
 # Create your views here.
 
 def employee_list(request):
-    return render(request, "employee_register/employee_list.html")
+    return render(request, "employeeregister/employee_list.html")
 
 
 # get and post api's from form
 def employee_form(request):
-    return render(request, "employee_register/employee_form.html")
+    form = EmployeeForm()
+    return render(request, "employeeregister/employee_form.html", {'form':form})
 
 #deleting a record
 def employee_delete(request):
