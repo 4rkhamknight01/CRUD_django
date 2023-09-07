be08d71b37126b700d10a9480d2b59b8162c67ce
4rkhamknight01 <devanshdasindia@outlook.com>
2023-09-07 11:09:31 +0530

    akjsfd

---
+++ crud_django/employee_register/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def employee_list(request):
+    return
+
+
+# get and post api's from form
+def employee_form(request):
+    return
+
+#deleting a record
+def employee_delete(request):
+    return
